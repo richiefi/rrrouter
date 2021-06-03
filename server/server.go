98350714f26bf5d42c27a07b84e01267f3e9e478
Kalle Karjalainen <kalle@richie.fi>
2021-06-03 11:31:25 +0300

    debug: Logging.

--- server/server.go
+++ server/server.go
@@ -355,6 +355,11 @@ func requestHandler(reqres *proxy.RequestResult, logger *apexlog.Logger, conf *c
 		defer reqres.Response.Body.Close()
 		logctx = logctx.WithField("proxiedURL", reqres.Response.Request.URL)
 
+		if w == nil {
+			// ch19238:
+			logctx.Errorf("writer has gone unexpectedly for %v", reqres.Response.Request.URL)
+			return
+		}
 		header := clearAndCopyHeaders(w, reqres.Response.Header, alwaysInclude)
 
 		var reader io.ReadCloser
