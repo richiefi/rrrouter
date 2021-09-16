6fca428a6acb422b926987cb89e276e21b33da27
Kalle Karjalainen <kalle@richie.fi>
2021-09-16 16:28:32 +0300

    rule: Scheme and host must be specified separately and if present, they must match the URL. A single wildcard can be placed at the end of the (path+query) pattern, which captures the rest of the matching string and is placed into the destination string, if a `$1` is present indicating the place to insert it in to.

--- proxy/rule.go
+++ proxy/rule.go
@@ -1,18 +1,18 @@
 package proxy
 
 import (
-	"bytes"
+	"errors"
 	"fmt"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
 // Rule describes a single forwarding rule
 type Rule struct {
 	enabled          bool
+	scheme           string
+	host             string
 	pattern          string
-	re               *regexp.Regexp
+	wci              []int
 	dest             string
 	internal         bool
 	methods          map[string]bool
@@ -41,34 +41,33 @@ const (
 )
 
 // NewRule builds a new Rule
-func NewRule(enabled bool, pattern, destination string, internal bool, methods map[string]bool, ruleType ruleType, hostHeader HostHeader,
+func NewRule(enabled bool, scheme, host, pattern, destination string, internal bool, methods map[string]bool, ruleType ruleType, hostHeader HostHeader,
 	recompression bool, cacheId string, forceRevalidate int, responseHeaders map[string]string, flattenRedirects bool, retryRule *Rule) (*Rule, error) {
+	if len(pattern) == 0 {
+		return nil, errors.New("Empty pattern")
+	}
+	if len(destination) == 0 {
+		return nil, errors.New("Empty destination")
+	}
 	lowpat := strings.ToLower(pattern)
-	addAnyProto := !(strings.HasPrefix(lowpat, "http://") || strings.HasPrefix(lowpat, "https://") || strings.HasPrefix(lowpat, "*://"))
-	inputParts := strings.Split(pattern, "*")
-	finalParts := make([]string, 0, len(inputParts)*3)
-	finalParts = append(finalParts, `(?i)\A`)
-	wildcardCount := 0
-	if addAnyProto {
-		finalParts = append(finalParts, "https?://")
-	}
-	for i, p := range inputParts {
-		qp := regexp.QuoteMeta(p)
-		finalParts = append(finalParts, qp)
-
-		if i < len(inputParts)-1 {
-			finalParts = append(finalParts, "(.*)")
-			wildcardCount++
+	firstIdx := strings.Index(lowpat, "*")
+	wci := make([]int, 0)
+	if firstIdx != -1 {
+		lastIdx := strings.LastIndex(lowpat, "*")
+		if firstIdx != lastIdx {
+			return nil, errors.New("Wildcard count in pattern > 1")
+		} else if firstIdx != len(lowpat)-1 {
+			return nil, errors.New("Wildcard must be placed as last character in the pattern")
 		}
+		wci = append(wci, firstIdx)
 	}
-	finalParts = append(finalParts, `\z`)
 
-	repattern := strings.Join(finalParts, "")
-	r := regexp.MustCompile(repattern)
 	rule := &Rule{
 		enabled:          enabled,
+		scheme:           scheme,
+		host:             host,
 		pattern:          pattern,
-		re:               r,
+		wci:              wci,
 		dest:             destination,
 		internal:         internal,
 		methods:          methods,
@@ -81,39 +80,6 @@ func NewRule(enabled bool, pattern, destination string, internal bool, methods m
 		flattenRedirects: flattenRedirects,
 		retryRule:        retryRule,
 	}
-
-	// First parse the main destination
-	ctx := &destinationParseContext{
-		destination:         rule.dest,
-		input:               destination,
-		replacePlaceholders: false,
-		maxPlaceholder:      -1,
-	}
-	_, err := parseDestinationWithContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-	if ctx.maxPlaceholder > wildcardCount {
-		return nil, fmt.Errorf("destination used placeholder %d but there are only %d wildcards in the pattern", ctx.maxPlaceholder, wildcardCount)
-	}
-
-	// And if there's a copy target, check that too
-	// if copyDest != "" {
-	// 	ctx := &destinationParseContext{
-	// 		destination:         rule.copyDest,
-	// 		input:               copyDest,
-	// 		replacePlaceholders: false,
-	// 		maxPlaceholder:      -1,
-	// 	}
-
-	// 	_, err = parseDestinationWithContext(ctx)
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	if ctx.maxPlaceholder > wildcardCount {
-	// 		return nil, fmt.Errorf("Copy destination used placeholder %d but there are only %d wildcards in the pattern", ctx.maxPlaceholder, wildcardCount)
-	// 	}
-	// }
 
 	return rule, nil
 }
@@ -138,114 +104,32 @@ func (r *Rule) String() string {
 	return fmt.Sprintf("Rule (%s,%s) %s %q -> %q", intext, rtype, methods, r.pattern, r.dest)
 }
 
-func (r *Rule) attemptMatch(s string) (*string, error) {
-	submatches := r.re.FindStringSubmatch(s)
-	if submatches == nil {
+func (r *Rule) attemptMatch(scheme, host, uri string) (*string, error) {
+	if (len(r.scheme) > 0 && r.scheme != scheme) || (len(r.host) > 0 && r.host != host) {
 		return nil, nil
 	}
 
-	ctx := &destinationParseContext{
-		destination:         r.dest,
-		input:               s,
-		inputMatches:        submatches,
-		replacePlaceholders: true,
-	}
-	dest, err := parseDestinationWithContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return &dest, nil
-}
-
-func parseDestinationWithContext(ctx *destinationParseContext) (string, error) {
-	var buf bytes.Buffer
-	nextState := stateLookForNextPlaceholder
-	var err error
-	for _, destrune := range ctx.destination {
-		nextState, err = nextState(&destrune, &buf, ctx)
-
-		if err != nil {
-			return "", err
+	if len(r.wci) == 1 {
+		pathLen := len(uri)
+		wcIdx := r.wci[0]
+		if pathLen-1 < wcIdx {
+			return nil, nil
 		}
-	}
-	_, err = nextState(nil, &buf, ctx)
-	if err != nil {
-		return "", err
-	}
 
-	return buf.String(), nil
-}
+		idx := strings.Index(uri, r.pattern[:wcIdx])
+		if idx != 0 {
+			return nil, nil
+		}
 
-type destinationParseContext struct {
-	destination         string
-	input               string
-	inputMatches        []string
-	maxPlaceholder      int
-	replacePlaceholders bool
-}
-
-type stateFunc func(r *rune, buf *bytes.Buffer, ctx *destinationParseContext) (stateFunc, error)
-
-func stateLookForNextPlaceholder(r *rune, buf *bytes.Buffer, ctx *destinationParseContext) (stateFunc, error) {
-	if r == nil {
+		captured := uri[wcIdx:]
+		d := strings.Replace(r.dest, "$1", captured, 1)
+		return &d, nil
+	} else if len(r.wci) == 0 {
+		if r.pattern == uri {
+			return &r.dest, nil
+		}
 		return nil, nil
+	} else {
+		return nil, errors.New("Wildcard count in pattern > 1")
 	}
-	switch *r {
-	case '$':
-		return stateReadPlaceholderNumber, nil
-	case '\\':
-		return stateReadLiteral, nil
-	default:
-		buf.WriteRune(*r)
-		return stateLookForNextPlaceholder, nil
-	}
-}
-
-func stateReadPlaceholderNumber(r *rune, buf *bytes.Buffer, ctx *destinationParseContext) (stateFunc, error) {
-	var numbuf bytes.Buffer
-	var numberAccumulator stateFunc
-
-	finishNumber := func(r *rune, buf *bytes.Buffer, ctx *destinationParseContext) (stateFunc, error) {
-		placeholderNum, err := strconv.Atoi(numbuf.String())
-		if err != nil {
-			return nil, err
-		}
-		ctx.maxPlaceholder = maxi(ctx.maxPlaceholder, placeholderNum)
-		if ctx.replacePlaceholders {
-			if placeholderNum >= len(ctx.inputMatches) {
-				return nil, fmt.Errorf("destination %q referred to placeholder %d out of bounds. Input %q, matches: %q", ctx.destination, placeholderNum, ctx.input, ctx.inputMatches)
-			}
-			buf.WriteString(ctx.inputMatches[placeholderNum])
-		}
-		return stateLookForNextPlaceholder(r, buf, ctx)
-	}
-
-	numberAccumulator = func(r *rune, buf *bytes.Buffer, ctx *destinationParseContext) (stateFunc, error) {
-		if r == nil {
-			return finishNumber(r, buf, ctx)
-		}
-		switch *r {
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			numbuf.WriteRune(*r)
-			return numberAccumulator, nil
-		default:
-			return finishNumber(r, buf, ctx)
-		}
-	}
-	return numberAccumulator(r, buf, ctx)
-}
-
-func maxi(i1, i2 int) int {
-	if i1 > i2 {
-		return i1
-	}
-	return i2
-}
-
-func stateReadLiteral(r *rune, buf *bytes.Buffer, ctx *destinationParseContext) (stateFunc, error) {
-	if r == nil {
-		return nil, fmt.Errorf("unfinished literal at the end of %q", ctx.destination)
-	}
-	buf.WriteRune(*r)
-	return stateLookForNextPlaceholder, nil
 }
