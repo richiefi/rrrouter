f6e32573cade2673089f13280db378d8219ebaf6
Kalle Karjalainen <2064810+kallekoo@users.noreply.github.com>
2022-07-07 18:05:02 +0300

    proxy: ForceAttemptHTTP2: false.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -125,7 +125,7 @@ func NewRouter(rules *Rules, logger *apexlog.Logger, conf *config.Config) *Route
 				PreferGo: true,
 			},
 		}).DialContext,
-		ForceAttemptHTTP2:     true,
+		ForceAttemptHTTP2:     false,
 		MaxConnsPerHost:       1000,
 		MaxIdleConns:          1000,
 		IdleConnTimeout:       10 * time.Second,
